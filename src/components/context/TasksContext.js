86c38eea61723ca4adf73d849ad0d13a0fac271b
BRajapandi <rajapandib29@gmail.com>
2023-09-07 17:08:10 +0530

    final context done

--- src/components/context/TasksContext.js
+++ src/components/context/TasksContext.js
@@ -2,6 +2,30 @@ import { createContext, useReducer } from "react";
 
 export const TasksContext = createContext();
 
+const tasksReducer = (state, action) => {
+  switch (action.type) {
+    case "ADD_TASK":
+      const id = Math.random() * 100;
+      let task = { ...action.payload, id };
+      return { ...state, taskList: [...state.taskList, task] };
+    case "REMOVE_TASK":
+      let list = state.taskList.filter((task) => task.id !== action.payload.id);
+      return { ...state, taskList: list };
+    case "UPDATE_TASK":
+      let updateList = state.taskList.map((task) =>
+        task.id === action.payload.id ? action.payload : task
+      );
+      return { ...state, taskList: [updateList] };
+    case "GET_TASKS":
+      return state.taskList;
+    case "SET_SELECTED_TASK":
+      return { ...state, selectedTask: { ...action.payload } };
+    case "GET_SELECTED_TASK":
+      return state.selectedTask;
+    default:
+      return state;
+  }
+};
 export const TasksContextProvider = ({ children }) => {
   const [state, dispatch] = useReducer(tasksReducer, {
     taskList: [],
--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import "bootstrap-icons/font/bootstrap-icons.css";  
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import { TasksContextProvider } from "./components/context/TasksContext";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <TasksContextProvider>
+      <App />
+    </TasksContextProvider>
   </React.StrictMode>
 );
 
